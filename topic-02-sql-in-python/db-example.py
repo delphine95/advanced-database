6a40cd081e22dfbe9034bf5acd6ce8ad6978bce6
Delphine RUZINDANA <48473728+delphine95@users.noreply.github.com>
2026-02-11 23:26:50 +0000

    DAtabase exercises

--- topic-02-sql-in-python/db-example.py
+++ topic-02-sql-in-python/db-example.py
@@ -1,118 +1,76 @@
 import argparse
 import sqlite3
 from pprint import pprint
-
 ap = argparse.ArgumentParser()
 ap.add_argument("--db", default="pets.db")
 args = ap.parse_args()
-
 connection = sqlite3.connect(args.db)
 
-print("succeeded in making connection.")
+print("Succeeded in making connection")
 
 connection.execute("drop table if exists pet")
 connection.commit()
-
-# list all the tables in the database
-cursor = connection.execute(
-    """
-    select name from sqlite_master
-    where type = 'table'
-    order by name
-    """
-)
-list_of_tables = [item[0] for item in cursor.fetchall()]
-print("the tables:")
-pprint(list_of_tables)
-
-connection.execute("""
-            create table pet
-            (
-                id integer primary key autoincrement,
-                name text not null,
-                kind text not null,
-                age integer,
-                food text
-            );
-            """)
-connection.commit()
-
-# list all the tables in the database
-cursor = connection.execute(
-    """
-    select name from sqlite_master
-    where type = 'table'
-    order by name
-    """
-)
-list_of_tables = [item[0] for item in cursor.fetchall()]
-print("the tables:")
-pprint(list_of_tables)
+print("dropped table pet (if it existed)")
 
 connection.execute(
-    "insert into pet (name, kind, age, food) values (?, ?, ?, ?)",
-    ("Dorothy", "dog", 11, "peanut butter"),
-)
-connection.execute(
-    "insert into pet (name, kind, age, food) values (?, ?, ?, ?)",
-    ("Whiskers", "hamster", 1, "hamster chow"),
-)
+    """
+create table pet(
 
+id integer primary key autoincrement,
+name text not null,
+kind text not null,
+age integer,
+food text);
+"""
+)
 connection.commit()
+print("created table pet")
 
-cursor = connection.execute("select sql from sqlite_master where type='table' and name=?",
-    ("pet",))
-row = cursor.fetchone()
-pprint(row[0] if row else "")
+connection.execute("insert into pet (name, kind, age, food) values (?,?,?,?)", ("Daily", "Dog", 10, "catty"), 
+)
+connection.commit()
+print("Inserted Daily")
 
-cursor = connection.execute("select * from pet")
-row = cursor.fetchone()
-pprint(row)
+connection.execute("insert into pet(name, kind, age, food) values (?,?,?,?)", ("Epson", "grey", 5, "Chicken"),)
+connection.commit()
+print("Inserted Epson")
 
-cursor = connection.execute("select * from pet")
-rows = cursor.fetchall()
-pprint(rows)
+connection.execute("insert into pet (name, kind, age, food) values (?,?,?,?)", ("Whiskey", "hamster", 4, "hamster cow"),)
+connection.commit()
+print("Inserted whiskers")
+
+connection.execute("delete from pet where name=?",("Whiskey",))
+connection.commit()
+print("deleted whiskey")
+
+connection.execute("update pet set age=? where name=?", (9, "Daily"))
+connection.commit()
+print("row updated")
 
 print("try with a wrong name")
 for i in [1,2]:
     try:
-        connection.execute("insert into petz (name, kind, age, food) values (?, ?, ?, ?)",("Sandy","cat",9,"tuna"))
+        connection.execute(
+            "insert into petz(name, kind, age, food) values (?,?,?,?)",("Sandy", "cat", 6, "tuna"),
+                           )
     except sqlite3.Error as e:
-        print("Caught sqlite error:", e)
+        print("caught sqlite error:", e)
 
 print("try with the correct name")
 for i in [1,2]:
     try:
-        connection.execute("insert into pet (name, kind, age, food) values (?, ?, ?, ?)",("Sandy","cat",9,"tuna"))
+        connection.execute(
+            "insert into pet (name, kind, age, food) values (?,?,?,?)", ("Sandy", "cat", 7, "tuna"),
+            
+        )
         connection.commit()
-        print("insert succeeded")
+        print("insert succeded")
         break
     except sqlite3.Error as e:
-        print("Caught sqlite error:", e)
+        print("caught sqlite error:", e)
 
-connection.execute(
-    "insert into pet (name, kind, age, food) values (?, ?, ?, ?)",
-    ("Maxwell", "cat", 11, "tuna"),
-)
-connection.execute(
-    "insert into pet (name, kind, age, food) values (?, ?, ?, ?)",
-    ("Stash", "mouse", 1, "cheese"),
-)
+cursor = connection.execute("select * from pet")
+rows = cursor.fetchall()
+print("rows in pet table:")
+pprint(rows)
 
-connection.commit()
-
-connection.execute(
-    "delete from pet where name=?",("Stash",)
-    )
-connection.commit()
-print("deletion complete")
-
-connection.execute(
-    "update pet set age=12, food='pretzels' where name=?",("Dorothy",)
-    )
-connection.commit()
-print("update complete")
-
-
-
-print("done.")
